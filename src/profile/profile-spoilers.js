ef21ca6463b07642b315cc1e4b8db07c73ded682
vsetia <setia.v@northeastern.edu>
2023-06-26 04:07:08 -0400

    fixing name display on profile

--- src/profile/profile-spoilers.js
+++ src/profile/profile-spoilers.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import ReviewCard from "../home/reviewcard";
-import { getAllReviewsForUserThunk } from "../services/profile-thunk";
+import { getAllReviewsForUserThunk, getUserThunk } from "../services/profile-thunk";
 import InfiniteScroll from "react-infinite-scroll-component"; 
-
+import { userName } from "../app/userSlice";
+import { useSelector } from "react-redux";
 function ProfileSpoilers({ profileId = "" , showToggles=true, slice=false, spoilerCount=0, setSpoilerCount="", modifiedId=null}) {
   let [data, setData] = useState([]);
   let [loading, setLoading] = useState(false);
   const [totalCount, setTotalCount] = useState(1000);
   const [pageNo, setPageNo] = useState(1);
   const limit = 3;
+  const logInName = useSelector(userName);
   const dispatch = useDispatch();
+  let[name, setName] = useState("");
   useEffect(() => {
+    async function loadProfile() {
+      const { payload } = await dispatch(getUserThunk(profileId));
+      setName(payload.data.name);
+    }
     async function fetchReviews() {
       let tlimit = limit
       if(slice)
@@ -25,8 +32,9 @@ function ProfileSpoilers({ profileId = "" , showToggles=true, slice=false, spoil
         setData(payload.data);
       setLoading(true);
     }
- 
+    loadProfile();
     fetchReviews();
+    
   }, [profileId,modifiedId]);
 
   const getMoreData = async () => {
@@ -44,7 +52,8 @@ function ProfileSpoilers({ profileId = "" , showToggles=true, slice=false, spoil
       <div className=" py-8">
             <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
               <h1 className="text-blue-500 text-4xl font-bold text-center animate-fade-in">
-              Your recent spoilers
+                {logInName===name ? "Your " : name+"'s "}
+               recent spoilers
               </h1>
             </div>
           </div>
